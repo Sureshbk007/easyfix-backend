657f54f212d902f6b7c865699ed63d07dc374887
Suresh BK <sureshbk.contact@gmail.com>
2024-09-17 16:18:08 +0545

    cors add

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import adminRoutes from "./routes/adminRoutes.js";
@@ -12,6 +13,7 @@ connectDB();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/admin", adminRoutes);
